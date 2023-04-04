d2685346a89b8e5f0b6e00adb90ba3409f577fd9
Kyle Schneider <kylesch115@gmail.com>
2023-04-04 09:53:28 -0600

    create routes for login/signup and home pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+
 function App() {
   return (
-    <h1>Chess Is Hard</h1>
+    <>
+      <Switch>
+
+        <Route exact path="/home">
+          <h1>Chess Is Hard</h1>
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+      </Switch>
+    </>
   );
 }
 
