21855589b9e25ad6204bb67fc653f53e73d701f2
Kyle Schneider <kylesch115@gmail.com>
2023-04-09 19:35:53 -0600

    rewrote pgn_to_fen more succinctly

--- server/chess/pgn_to_fen.py
+++ server/chess/pgn_to_fen.py
@@ -26,24 +26,87 @@ def pgn_to_dict(pgn):
     pgn_dict['move_list'] = move_list
 
     return pgn_dict
-            
-def update_fen(fen, from_index, to_index):
-    fen_list = util.fen_to_list(fen)
-    piece = fen_list[from_index]
-    fen_list[from_index] = None
-    fen_list[to_index] = piece
-    fen = util.list_to_fen(fen_list)
-    return fen
 
-def update_fen_promotion(fen, to_index, promotion_type, whites_turn):
-    fen_list = util.fen_to_list(fen)
-    if whites_turn:
-        fen_list[to_index + 8] = None
+def update_fen(fen, from_index, to_index, promotion_type=None):
+    fen_dict = util.fen_to_dict(fen)
+    piece_placement_list = util.piece_placement_to_list(fen_dict['piece_placement'])
+    piece_type = piece_placement_list[from_index]
+    
+    new_fen_dict = fen_dict.copy()
+
+    # piece_placement
+    new_piece_placement_list = piece_placement_list.copy()
+    new_piece_placement_list[to_index] = piece_placement_list[from_index]
+    new_piece_placement_list[from_index] = None
+    # promotion
+    if promotion_type:
+        new_piece_placement_list[to_index] = promotion_type
+    # castling
+    if piece_type == 'K':
+        if from_index == 60 and to_index == 62:
+            new_piece_placement_list[61] = 'R'
+            new_piece_placement_list[63] = None
+        if from_index == 60 and to_index == 58:
+            new_piece_placement_list[59] = 'R'
+            new_piece_placement_list[56] = None
+    if piece_type == 'k':
+        if from_index == 4 and to_index == 6:
+            new_piece_placement_list[5] = 'r'
+            new_piece_placement_list[7] = None
+        if from_index == 4 and to_index == 2:
+            new_piece_placement_list[3] = 'r'
+            new_piece_placement_list[0] = None
+    # en passant
+    if fen_dict['en_passant_target_square'] != '-':
+        en_passant_target_index = util.square_to_index(fen_dict['en_passant_target_square'])
+        if piece_type == 'P' and to_index == en_passant_target_index :
+            new_piece_placement_list[en_passant_target_index + 8] = None
+        if piece_type == 'p' and to_index == en_passant_target_index :
+            new_piece_placement_list[en_passant_target_index - 8] = None
+    new_fen_dict['piece_placement'] = util.piece_placement_list_to_string(new_piece_placement_list)
+
+    # active_color
+    if fen_dict['active_color'] == 'w':
+        new_fen_dict['active_color'] = 'b'
+    else:  
+        new_fen_dict['active_color'] = 'w'
+
+    # castling_availability
+    castling_availability = fen_dict['castling_availability']
+    if piece_type == 'K':
+        new_fen_dict['castling_availability'] = castling_availability.replace('K', '').replace('Q', '')
+    if piece_type == 'k':
+        new_fen_dict['castling_availability'] = castling_availability.replace('k', '').replace('q', '')
+    if piece_type == 'R' and from_index == 63:
+        new_fen_dict['castling_availability'] = castling_availability.replace('K', '')
+    if piece_type == 'R' and from_index == 56:
+        new_fen_dict['castling_availability'] = castling_availability.replace('Q', '')
+    if piece_type == 'r' and from_index == 7:
+        new_fen_dict['castling_availability'] = castling_availability.replace('k', '')
+    if piece_type == 'r' and from_index == 0:
+        new_fen_dict['castling_availability'] = castling_availability.replace('q', '')
+    if new_fen_dict['castling_availability'] == '':
+        new_fen_dict['castling_availability'] = '-'
+
+    # en_passant_target_square
+    if piece_type.upper() == 'P' and abs(to_index - from_index) == 16:
+        en_passant_target_index = min(to_index, from_index) + 8
+        en_passant_target_square = util.index_to_square(en_passant_target_index)
+        new_fen_dict['en_passant_target_square'] = en_passant_target_square
     else:
-        fen_list[to_index - 8] = None
-    fen_list[to_index] = promotion_type
-    fen = util.list_to_fen(fen_list)
-    return fen
+        new_fen_dict['en_passant_target_square'] = '-'
+
+    # halfmove_clock
+    if piece_type.upper() == 'P' or piece_placement_list[to_index] != None:
+        new_fen_dict['halfmove_clock'] = 0
+    else:
+        new_fen_dict['halfmove_clock'] = fen_dict['halfmove_clock'] + 1
+
+    # fullmove_number
+    if fen_dict['active_color'] == 'b':
+        new_fen_dict['fullmove_number'] = fen_dict['fullmove_number'] + 1
+
+    return util.fen_from_dict(new_fen_dict)
 
 def pgn_to_fen(pgn):
     fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
@@ -54,127 +117,22 @@ def pgn_to_fen(pgn):
     move_list = move_list.split(' ')
     move_list = [m for m in move_list if m[0].isalpha()]
 
-    whites_turn = True
-    castling_rights = 'KQkq'
-    half_move = 0
-    full_move = 1
-    en_passant = '-'
-    for move in  move_list:
-        # print(fen)
-        # print(move)
-        promotion = False
-        from_index, to_index = util.algebraic_to_index(fen, whites_turn, move)
-
+    for move in move_list:
+        from_index, to_index = util.algebraic_to_index(fen, move)
         # promotion
-        if type(from_index) == str:
-            fen = update_fen_promotion(fen, to_index, from_index, whites_turn)
-            promotion = True
-        fen_list = util.fen_to_list(fen)
-        if not promotion:
-            piece = fen_list[from_index]
-            target_piece = fen_list[to_index]
+        if type(from_index) == tuple:
+            promotion_type = to_index
+            from_index, to_index = from_index
+            fen = update_fen(fen, from_index, to_index, promotion_type)
         else:
-            piece = 'P' if whites_turn else 'p'
-            target_piece = None
-
-        # castling  
-        if from_index == 60 and to_index == 62:
-            if fen_list[60] == 'K':
-                fen = update_fen(fen, 63, 61)
-                castling_rights = castling_rights.replace('K', '')
-                castling_rights = castling_rights.replace('Q', '')
-        if from_index == 60 and to_index == 58:
-            if fen_list[60] == 'K':
-                fen = update_fen(fen, 56, 59)
-                castling_rights = castling_rights.replace('K', '')
-                castling_rights = castling_rights.replace('Q', '')
-        if from_index == 4 and to_index == 6:
-            if fen_list[4] == 'k':
-                fen = update_fen(fen, 7, 5)
-                castling_rights = castling_rights.replace('k', '')
-                castling_rights = castling_rights.replace('q', '')
-        if from_index == 4 and to_index == 2:
-            if fen_list[4] == 'k':
-                fen = update_fen(fen, 0, 3)
-                castling_rights = castling_rights.replace('k', '')
-                castling_rights = castling_rights.replace('q', '')
-
-        # castling rights   
-        if piece == 'K':
-            castling_rights = castling_rights.replace('K', '')
-            castling_rights = castling_rights.replace('Q', '')
-        if piece == 'k':
-            castling_rights = castling_rights.replace('k', '')
-            castling_rights = castling_rights.replace('q', '')
-        if piece == 'R' and from_index == 56:
-            castling_rights = castling_rights.replace('Q', '')
-        if piece == 'R' and from_index == 63:
-            castling_rights = castling_rights.replace('K', '')
-        if piece == 'r' and from_index == 0:
-            castling_rights = castling_rights.replace('q', '')
-        if piece == 'r' and from_index == 7:
-            castling_rights = castling_rights.replace('k', '')
-
-        # apply en passant
-        if en_passant != '-':
-            en_peasent_file = ord(en_passant[0]) - 96
-            en_peasent_rank = int(en_passant[1])
-            en_peasent_index = util.filerank_to_index(en_peasent_file, en_peasent_rank)
-            if piece == 'P' and to_index == en_peasent_index:
-                fen_list[en_peasent_index + 8] = None
-                fen = util.list_to_fen(fen_list)
-            if piece == 'p' and to_index == en_peasent_index:
-                fen_list[en_peasent_index - 8] = None
-                fen = util.list_to_fen(fen_list)
-
-        # update fen
-        if not promotion:
             fen = update_fen(fen, from_index, to_index)
-        # set player turn
-        whites_turn = not whites_turn
-        turn = 'w' if whites_turn else 'b'
-        # set castling rights
-        if castling_rights == '':
-            castling_rights = '-'
-        # set full move
-        if turn == 'w':
-            full_move += 1
-        # set halfmove
-        if piece == 'p' or piece == 'P':
-            half_move = 0
-        if target_piece != None:
-            half_move = 0
-        # set en passant
-        en_passant = '-'
-        if not promotion:
-            if piece == 'p' or piece == 'P':
-                if abs(from_index - to_index) == 16 or abs(to_index - from_index) == 16:
-                    index = min(from_index, to_index) + 8
-                    file, rank = util.index_to_filerank(index)
-                    file = chr(file + 96)
-                    en_passant = f'{file}{rank}'
-
-        fen += f' {turn} {castling_rights} {en_passant} {half_move} {full_move}' 
-
-        half_move += 1
-
+        
     return fen
 
-if __name__ == '__main__':
-    # pgn = '''[Event "?"]
-    # [Site "?"]
-    # [Date "????.??.??"]
-    # [Round "?"]
-    # [White "?"]
-    # [Black "?"]
-    # [Result "*"]
+# if __name__ == '__main__':
+#     pgn = '[Event \"Mainz Rapid Final\"]\n[Site \"Mainz GER\"]\n[Date \"2007.08.19\"]\n[EventDate \"2007.08.19\"]\n[Round \"4\"]\n[Result \"0-1\"]\n[White \"The Levonator\"]\n[Black \"Vishy\"]\n[ECO \"E06\"]\n[WhiteElo \"2750\"]\n[BlackElo \"2792\"]\n[PlyCount \"136\"]\n\n1. d4 Nf6 2. c4 e6 3. g3 d5 4. Nf3 Be7 5. Bg2 O-O 6. O-O dxc4\n7. Qc2 a6 8. Qxc4 b5 9. Qc2 Bb7 10. Bd2 Ra7 11. Rc1 Be4\n12. Qb3 Nc6 13. e3 Qa8 14. Qd1 b4 15. Qf1 Rd8 16. Be1 a5\n17. a3 Bd6 18. Nfd2 Bxg2 19. Qxg2 Ra6 20. Nc4 Nd5 21. Nbd2\nbxa3 22. bxa3 Be7 23. Rab1 a4 24. Qf1 Nb8 25. Ne4 Nd7 26. Nc3\nc5 27. dxc5 Nxc3 28. Bxc3 Nxc5 29. Bb4 Bf8 30. Rd1 Rc8 31. Ne5\nRa7 32. Rbc1 Rac7 33. Qg2 Qxg2+ 34. Kxg2 f6 35. Nf3 e5 36. Ne1\nKf7 37. Rc4 Nb3 38. Rh4 Bxb4 39. Rxb4 Ra7 40. Kf3 Rc3 41. Nd3\nRc2 42. Rb6 Ra2 43. Nb4 Rxa3 44. Nc6 Rc7 45. Nd8+ Kg6 46. Ne6\nRe7 47. g4 Rxe6 48. Rxe6 Nd4+ 49. Rxd4 exd4 50. Re4 dxe3\n51. fxe3 Ra1 52. h3 a3 53. Ra4 a2 54. Kg2 Kf7 55. Ra6 Ke7\n56. e4 Kd7 57. h4 h6 58. g5 hxg5 59. hxg5 Kc7 60. Kh2 Kb7\n61. Ra3 Kc6 62. gxf6 gxf6 63. Rc3+ Kb5 64. Rc2 Kb4 65. Rf2\nRh1+ 66. Kxh1 a1=Q+ 67. Kg2 Kc4 68. Kf3 Kd3 0-1'
+#     fen = pgn_to_fen(pgn)
+#     print(fen)
 
-    # 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 b5 5. Bb3 Nf6 6. Qe2 Nxe4 7. Qxe4 Bc5 8.
-    # Nxe5 Nxe5 9. Qxe5+ Kf8 10. d3 Qg5 11. Bxg5 Bxf2+ 12. Kxf2 Bb7 13. Nc3 Bxg2 14.
-    # Kxg2 Re8 15. Rhe1 Rxe5 16. Rxe5 d6 17. Rae1 dxe5 18. Rxe5 *'''
+#     pass
 
-    pgn = '[Event \"Let\\\\\'s Play!\"]\n[Site \"Chess.com\"]\n[Date \"2023.03.27\"]\n[Round \"?\"]\n[White \"Kye_Schnei\"]\n[Black \"topherLud\"]\n[Result \"*\"]\n[ECO \"C42\"]\n[WhiteElo \"1098\"]\n[BlackElo \"381\"]\n[TimeControl \"1/86400\"]\n\n1. e4 e5 2. Nf3 Nf6 3. Nxe5 Bd6 4. Nf3 O-O 5. d4 Nxe4 6. Bd3 Qh4 7. Nxh4 f5 8.\nQf3 Nc6 9. O-O Nxd4 10. Qe3 g5 11. Bxe4 gxh4 12. Bd5+ Ne6 13. Nc3 f4 14. Qe4 c6\n15. Bc4 b5 16. Bxe6+ dxe6 17. Qxc6 Rb8 18. Qxd6 Rb6 19. Qe5 Rf5 20. Qd4 h3 21. Ne2 *'   
-    # pgn = '[Event \"Let\\\\\'s Play!\"]\n[Site \"Chess.com\"]\n[Date \"2023.03.27\"]\n[Round \"?\"]\n[White \"Kye_Schnei\"]\n[Black \"topherLud\"]\n[Result \"*\"]\n[ECO \"C42\"]\n[WhiteElo \"1098\"]\n[BlackElo \"381\"]\n[TimeControl \"1/86400\"]\n\n1. e4 e5 2. Nf3 Nf6 3. Nxe5 Bd6 4. Nf3 O-O 5. d4 Nxe4 6. Bd3 Qh4 7. Nxh4 f5 8.\nQf3 Nc6 9. O-O Nxd4 10. Qe3 g5 11. Bxe4 gxh4 12. Bd5+ Ne6 13. Nc3 f4 14. Qe4 c6\n15. Bc4 b5 16. Bxe6+ dxe6 17. Qxc6 Rb8 18. Qxd6 Rb6 19. Qe5 Rf5 20. Qd4 *'
-    x = pgn_to_fen(pgn)
-    print('final position:')
-    print(x)
